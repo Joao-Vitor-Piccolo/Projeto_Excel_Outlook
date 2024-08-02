a3b3a351b773a8d07a25200729a424a9fb6a3604
Piccolo's <joaovitorpiccolo015@gmail.com>
2024-08-02 10:06:56 -0300

    Sessão de exportar
    Criei uma parte que exporta os dados para o excel, assim criando uma planilha

--- Projeto Excel Outlook/main.py
+++ Projeto Excel Outlook/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from openpyxl import load_workbook as load_wb
 import win32com.client as win32
 
 # -=-=-=--=--=--=--=--=--=--=--=--=--=-=--=--Importar--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-
@@ -11,13 +12,25 @@ emails = inbox.Items
 emails = emails.Restrict("[MessageClass]='IPM.Note'")  # Filtro para pegar apenas emails.
 
 data = datetime(2024, 8, 1)
-data = data.strftime('%d/%m/%Y')
-data = f"[ReceivedTime] >= '{data}'"
 
-emails = emails.Restrict(data)
+data = data.strftime('%d/%m/%Y')
+
+dataf = f"[ReceivedTime] >= '{data}'"
+
+emails = emails.Restrict(dataf)
 n_mensagens = emails.Count
 
 print(f'Seu numero de emails no inbox: {n_mensagens}')
 
 # -=-=-=--=--=--=--=--=--=--=--=--=--=--=--=--Exportar--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=--=-
 
+wb = load_wb(r'C:\Users\jvpic\OneDrive\Área de Trabalho\Trabalhos\Book3.xlsx')
+ws = wb['Sheet1']
+
+if ws['A2']:
+    ws.insert_rows(2)
+
+ws['A2'] = str(data) + ':'
+ws['B2'] = str(n_mensagens)
+
+wb.save(r'C:\Users\jvpic\OneDrive\Área de Trabalho\Trabalhos\Book3.xlsx')
